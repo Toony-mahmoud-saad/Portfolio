29162f0db6e824c0a0786d51a6f067f45490b281
toony <2mahmoudsaadz@gmail.com>
2025-10-19 15:11:51 +0300

    create software engineer portfolio

---
+++ script.js
@@ -0,0 +1,30 @@
+var fullStackRadio = document.getElementById("btnradio1")
+var backEndRadio = document.getElementById("btnradio2")
+var frontEndRadio = document.getElementById("btnradio3")
+
+var fullBrojects = document.getElementById("fullBrojects")
+var backBrojects = document.getElementById("backBrojects")
+var frontBrojects = document.getElementById("frontBrojects")
+
+fullStackRadio.onclick= ()=>{
+  fullBrojects.classList.remove('d-none')
+  backBrojects.classList.add('d-none')
+  frontBrojects.classList.add('d-none')
+}
+
+backEndRadio.onclick = ()=>{
+  backBrojects.classList.remove('d-none')
+  fullBrojects.classList.add('d-none')
+  frontBrojects.classList.add('d-none')
+}
+
+frontEndRadio.onclick = ()=>{
+  frontBrojects.classList.remove('d-none')
+  backBrojects.classList.add('d-none')
+  fullBrojects.classList.add('d-none')
+}
+
+
+
+
+
